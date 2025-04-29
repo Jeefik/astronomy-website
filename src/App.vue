3276009d71e66e4599940b39b1a75fa57e95aebd
Jeefik <jeefik55@gmail.com>
2025-04-29 16:37:27 +0600

    newicons

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <template>
   <div id="app" class="min-h-screen min-w-full flex flex-col font-sans">
     <!-- Шапка -->
-    <header
-      class="bg-gradient-to-r from-gray-600 to-sky-600 text-white p-4 fixed top-0 left-0 w-full shadow-lg"
-    >
+    <header class="bg-gradient-to-r from-gray-600 to-sky-600 text-white p-4 fixed top-0 left-0 w-full shadow-lg">
       <div class="container mx-auto flex justify-between items-center">
         <h1
-          class="text-3xl font-bold tracking-wide text-transparent bg-clip-text bg-gradient-to-r from-gray-300 to-sky-400"
-        >
+          class="text-3xl font-bold tracking-wide text-transparent bg-clip-text bg-gradient-to-r from-gray-300 to-sky-400">
           Юлия Свидзинская
         </h1>
 
@@ -16,17 +13,32 @@
 
         <!-- Навигация на больших экранах -->
         <nav class="space-x-3 gap-2.5 hidden lg:flex">
-          <a href="#" class="hover:underline text-lg">Главная</a>
-          <a href="#" class="hover:underline text-lg">Услуги</a>
-          <a href="#" class="hover:underline text-lg">Обо мне</a>
-          <a href="#" class="hover:underline text-lg">Контакты</a>
+          <a href="#" class="text-lg relative group">
+            Главная
+            <span
+              class="absolute left-0 bottom-0 w-full h-0.5 bg-blue-200 scale-x-0 transition-all duration-300 group-hover:scale-x-100"></span>
+          </a>
+          <a href="#" class="text-lg relative group">
+            Услуги
+            <span
+              class="absolute left-0 bottom-0 w-full h-0.5 bg-blue-200 scale-x-0 transition-all duration-300 group-hover:scale-x-100"></span>
+          </a>
+          <a href="#" class="text-lg relative group">
+            Обо мне
+            <span
+              class="absolute left-0 bottom-0 w-full h-0.5 bg-blue-200 scale-x-0 transition-all duration-300 group-hover:scale-x-100"></span>
+          </a>
+          <a href="#" class="text-lg relative group">
+            Контакты
+            <span
+              class="absolute left-0 bottom-0 w-full h-0.5 bg-blue-200 scale-x-0 transition-all duration-300 group-hover:scale-x-100"></span>
+          </a>
         </nav>
 
+
         <!-- Выпадающее меню для мобильных -->
-        <nav
-          v-if="menuOpen"
-          class="lg:hidden absolute top-16 right-0 w-2/3 bg-sky-800 text-white p-4 transition-all duration-500 ease-in-out transform"
-        >
+        <nav v-if="menuOpen"
+          class="lg:hidden absolute top-16 right-0 w-2/3 bg-sky-800 text-white p-4 transition-all duration-500 ease-in-out transform">
           <a href="#" class="block py-2 hover:underline">Главная</a>
           <a href="#" class="block py-2 hover:underline">Услуги</a>
           <a href="#" class="block py-2 hover:underline">Обо мне</a>
@@ -99,15 +111,27 @@
 
     <!-- Футер -->
     <footer
-      class="bg-gradient-to-r from-sky-600 to-gray-800 text-center py-6 text-white p-4 fixed top-0 left-0 w-screen shadow-lg"
-    >
+      class="bg-gradient-to-r from-sky-600 to-gray-800 text-center py-6 text-white p-4 fixed bottom-0 left-0 w-screen shadow-lg">
       <p>© 2025 Астролог Юлия Свидзинская. Все права защищены.</p>
-      <div class="flex justify-center space-x-6 mt-4">
-        <a href="#" class="text-blue-300 hover:text-blue-500">Instagram</a>
-        <a href="#" class="text-blue-300 hover:text-blue-500">Telegram</a>
-        <a href="#" class="text-blue-300 hover:text-blue-500">Email</a>
+      <div class="flex justify-center space-x-6 mt-4 gap-4">
+        <a href="https://www.instagram.com/yulyaaach/" class="text-blue-300 relative group text-2xl">
+          <i class="fab fa-instagram"></i>
+          <span
+            class="absolute left-0 bottom-0 w-full h-0.5 bg-blue-200 scale-x-0 transition-all duration-300 group-hover:scale-x-100"></span>
+        </a>
+        <a href="https://t.me/Yulya_Svidzinskaya" class="text-blue-300 relative group text-2xl">
+          <i class="fab fa-telegram-plane"></i>
+          <span
+            class="absolute left-0 bottom-0 w-full h-0.5 bg-blue-200 scale-x-0 transition-all duration-300 group-hover:scale-x-100"></span>
+        </a>
+        <a href="mailto:yulya_chernikova_97@icloud.com" class="text-blue-300 relative group text-2xl">
+          <i class="fas fa-envelope"></i>
+          <span
+            class="absolute left-0 bottom-0 w-full h-0.5 bg-blue-200 scale-x-0 transition-all duration-300 group-hover:scale-x-100"></span>
+        </a>
       </div>
     </footer>
+
   </div>
 </template>
 
@@ -126,7 +150,9 @@ const toggleMenu = () => {
 @import url('https://fonts.googleapis.com/css2?family=Lora:wght@700&family=Roboto:wght@400&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Tagesschrift&display=swap');
 
-html, body, #app {
+html,
+body,
+#app {
   height: 100%;
   width: 100%;
   margin: 0;
@@ -147,14 +173,17 @@ header h1 {
 }
 
 main {
-  padding-top: 120px; /* Для того, чтобы контент не перекрывал шапку */
-  padding-bottom: 60px; /* Для футера */
+  padding-top: 120px;
+  /* Для того, чтобы контент не перекрывал шапку */
+  padding-bottom: 60px;
+  /* Для футера */
   flex-grow: 1;
   overflow-y: auto;
   display: flex;
   justify-content: center;
   align-items: flex-start;
-  background-color: #f7f7f7; /* Светлый фон для контента */
+  background-color: #f7f7f7;
+  /* Светлый фон для контента */
 }
 
 main .flex {
@@ -164,7 +193,7 @@ main .flex {
   gap: 2rem;
 }
 
-main .space-y-8 > section {
+main .space-y-8>section {
   margin-bottom: 2rem;
   padding: 2rem;
   border-radius: 16px;
